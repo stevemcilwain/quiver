1214e6e96248d6f257d41ead8f02aebaa87384e8
Steve M <gulfsteve@gmail.com>
2020-02-13 02:01:23 -0600

    fix gh

--- modules/qq-recon-github.zsh
+++ modules/qq-recon-github.zsh
@@ -26,7 +26,7 @@ qq-recon-github-top5() {
 }
 
 
-GH_TOP5 << END
+GH_TOP5= << END
 security_credentials
 connectionstring
 ssh2_auth_password
@@ -34,123 +34,123 @@ send_keys
 send,keys
 END
 
-GH_AWS << END
-AKIA
-amazon
-s3cfg
-AWS
-S3
-bucket
-cloudfront
-END
+# GH_AWS << END
+# AKIA
+# amazon
+# s3cfg
+# AWS
+# S3
+# bucket
+# cloudfront
+# END
 
-GH_API << END
-access_token
-API Secret
-access_secret
-api_key
-client_secret
-consumer_secret
-customer_secret
-user_secret
-secret_key
-END
+# GH_API << END
+# access_token
+# API Secret
+# access_secret
+# api_key
+# client_secret
+# consumer_secret
+# customer_secret
+# user_secret
+# secret_key
+# END
 
-GH_KEYS << END
------BEGIN RSA PRIVATE KEY-----
------BEGIN EC PRIVATE KEY-----
------BEGIN PRIVATE KEY-----
------BEGIN PGP PRIVATE KEY BLOCK-----
-END
+# GH_KEYS << END
+# -----BEGIN RSA PRIVATE KEY-----
+# -----BEGIN EC PRIVATE KEY-----
+# -----BEGIN PRIVATE KEY-----
+# -----BEGIN PGP PRIVATE KEY BLOCK-----
+# END
 
-GH_B2B << END
-EAA
-EAACEd
-EAACEdEose0cBA 
-AIza
-.apps.googleusercontent.com
-sq0atp
-sq0csp
-key-
-sk_live_ 
-rk_live_
-END
+# GH_B2B << END
+# EAA
+# EAACEd
+# EAACEdEose0cBA 
+# AIza
+# .apps.googleusercontent.com
+# sq0atp
+# sq0csp
+# key-
+# sk_live_ 
+# rk_live_
+# END
 
-GH_COMMS << END
-removed prod
-deleted prod
-removed data
-deleted data
-sanitized 
-hardcoded
-production
-staging
-sensitive
-insecure
-unsecure
-vulnerability
-encrypted
-END
+# GH_COMMS << END
+# removed prod
+# deleted prod
+# removed data
+# deleted data
+# sanitized 
+# hardcoded
+# production
+# staging
+# sensitive
+# insecure
+# unsecure
+# vulnerability
+# encrypted
+# END
 
-GH_CLOUD << END
-AMAZON
-AWS
-APIARY
-CLOUDFLARE
-CLOUDANT
-CONTENTFUL
-DIGITALOCEAN
-DOCKER
-FIREBASE
-GCLOUD
-HEROKU
-LINODE
-NETLIFY
-NGROK
-SALESFORCE
-WATSON
-OPENSTACK
-END
+# GH_CLOUD << END
+# AMAZON
+# AWS
+# APIARY
+# CLOUDFLARE
+# CLOUDANT
+# CONTENTFUL
+# DIGITALOCEAN
+# DOCKER
+# FIREBASE
+# GCLOUD
+# HEROKU
+# LINODE
+# NETLIFY
+# NGROK
+# SALESFORCE
+# WATSON
+# OPENSTACK
+# END
 
-GH_AUTH << END
-AUTH0
-OKTA
-END
+# GH_AUTH << END
+# AUTH0
+# OKTA
+# END
 
-GH_DOTFILES << END
-filename:bash_history
-filename:bash_profile
-filename:bashrc
-filename:zshrc
-filename:zsh_history
-filename:.sh_history
-filename:robomongo.json
-filename:id_rsa
-filename:id_dsa
-filename:.dockercfg auth
-filename:filezilla.xml Pass
-END
-
-
-GH_CMS << END
-filename:wp-config
-filename:wp-config.php
-WORDPRESS_DB
-MAGENTO_AUTH
-MAGENTO_AUTH
-MAGENTO_PASSWORD
-CONTENTFUL_
-END
+# GH_DOTFILES << END
+# filename:bash_history
+# filename:bash_profile
+# filename:bashrc
+# filename:zshrc
+# filename:zsh_history
+# filename:.sh_history
+# filename:robomongo.json
+# filename:id_rsa
+# filename:id_dsa
+# filename:.dockercfg auth
+# filename:filezilla.xml Pass
+# END
 
 
-GH_EXT << END
-extension:bat
-extension:json client_secret
-extension:json mongolab.com
-extension:pem private
-extension:ppk private
-extension:sh
-extension:sql mysql dump password
-extension:yaml mongolab.com
-extension:zsh
-END
+# GH_CMS << END
+# filename:wp-config
+# filename:wp-config.php
+# WORDPRESS_DB
+# MAGENTO_AUTH
+# MAGENTO_AUTH
+# MAGENTO_PASSWORD
+# CONTENTFUL_
+# END
+
+
+# GH_EXT << END
+# extension:bat
+# extension:json client_secret
+# extension:json mongolab.com
+# extension:pem private
+# extension:ppk private
+# extension:sh
+# extension:sql mysql dump password
+# extension:yaml mongolab.com
+# extension:zsh
+# END
