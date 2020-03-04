8abb66c0c5302d6cfc433d84953e086fe47e13b7
Steve M <gulfsteve@gmail.com>
2020-03-04 12:05:44 -0600

    Delete qq-custom.zsh

--- modules/qq-custom.zsh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env zsh
-
-############################################################# 
-# qq-custom
-#############################################################
-
-# Use this module to add custom aliases and functions, it will
-# ignored in .gitignore
